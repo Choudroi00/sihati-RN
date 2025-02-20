c064f56f4a820cdb766417455db096b5c850f8c1
chibo <bayoubbelmiloud@gmail.com>
2025-02-20 01:54:35 +0100

    docker builder fixed

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,13 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require("@react-native/metro-config");
 const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
 const defaultConfig = getDefaultConfig(__dirname);
-const mergedConfig = mergeConfig(defaultConfig, {}); // Merging with an empty object is redundant but keeps the structure clear
 
-const windWrappedConfig = withNativeWind(mergedConfig, { input: "./global.css" });
+// Apply NativeWind first
+const windWrappedConfig = withNativeWind(defaultConfig, { input: "./global.css" });
+
+// Then apply Reanimated
 const reanimatedWrappedConfig = wrapWithReanimatedMetroConfig(windWrappedConfig);
 
 module.exports = reanimatedWrappedConfig;
